353b3f9636f54d50eec6390d7039ebb62312e726
Daniel McGrath <demcgrath2@gmail.com>
2018-05-13 17:24:19 -0700

    Update BinaryTree.js

--- BinaryTree.js
+++ BinaryTree.js
@@ -1,6 +1,6 @@
 //              3
 //            /   \
-//          30     15
+//          30     10
 //         /  \   /  \
 //        1.   6 15   7
 //                     \
@@ -15,7 +15,7 @@ var r = new TreeNode(3);
 r.left = new TreeNode(30);
 r.left.left = new TreeNode(1);
 r.left.right = new TreeNode(6);
-r.right = new TreeNode(15);
+r.right = new TreeNode(10);
 r.right.left = new TreeNode(15)
 r.right.right = new TreeNode(7)
 r.right.right.right = new TreeNode(9)
