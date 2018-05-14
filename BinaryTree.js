9c1a139a9aef4eb08c4744dd5497549831065ae4
Daniel McGrath <demcgrath2@gmail.com>
2018-05-13 17:13:16 -0700

    Binary Tree construction example

---
+++ BinaryTree.js
@@ -0,0 +1,21 @@
+//              3
+//            /   \
+//          30     15
+//         /  \   /  \
+//        1.   6 15   7
+//                     \
+//                      9
+
+function TreeNode(val) {
+     this.val = val;
+     this.left = this.right = null;
+}
+
+var r = new TreeNode(3);
+r.left = new TreeNode(30);
+r.left.left = new TreeNode(1);
+r.left.right = new TreeNode(6);
+r.right = new TreeNode(15);
+r.right.left = new TreeNode(15)
+r.right.right = new TreeNode(7)
+r.right.right.right = new TreeNode(9)
