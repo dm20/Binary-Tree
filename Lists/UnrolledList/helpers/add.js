a557d607f522783ed4c4c38ee967542a3d98fb57
Daniel <demcgrath2@gmail.com>
2018-10-16 21:16:17 -0700

    Update add.js

--- Lists/UnrolledList/helpers/add.js
+++ Lists/UnrolledList/helpers/add.js
@@ -1,15 +1,15 @@
 var add = function(node, index, item) {
   var count = 0;
-  UnrolledListNode n = node;
+  n = node;
   var stop = false;
   
   while (n != null && !stop) {
     if (index <= count + n.vals.length) {
       if (n.vals.length < MAX_LEN) {
         var newVals = []
-        newVals.push(n.vals.slice(0, i - count,))
+        newVals.push(n.vals.slice(0, i - count))
         newVals.push(item)
-        newVals.push(n.vals.slice(i - count,n.vals.length)
+        newVals.push(n.vals.slice(i - count,n.vals.length))
         if (newVals.length > n.MAX_LEN) {
           n.vals = newVals.slice(0,n.MAX_LEN)
           newNode = new UnrolledListNode(newVals.slice(n.MAX_LEN, newVals.length), n.MAX_LEN)
